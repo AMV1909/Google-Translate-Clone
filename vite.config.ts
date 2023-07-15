c07b46c31d9f659da1dc18bbac9c3f51bec2cecf
Axel <amorales264@unab.edu.co>
2023-07-15 17:09:38 -0500

    gh-pages added to the project

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
+    base: "https://amv1909.github.io/Google-Translate-Clone/",
     test: {
         environment: "happy-dom",
     },
